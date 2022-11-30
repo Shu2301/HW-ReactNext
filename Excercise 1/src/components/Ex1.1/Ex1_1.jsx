4c023451c3d6bb9b30141e038050440094c2fde1
Shu <thanhtrung230100@gmail.com>
2022-11-30 18:03:53 +0700

    update

---
+++ Excercise 1/src/components/Ex1.1/Ex1_1.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+function fetchUser() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({ id: 23, name: 'Shu' });
+        }, 1000);
+    });
+}
+
+export default function Ex1_1() {
+    const [id, setId] = React.useState('loading...');
+    const [name, setName] = React.useState('loading...');
+
+    React.useEffect(() => {
+        fetchUser().then((user) => {
+            setId(user.id);
+            setName(user.name);
+        });
+    });
+
+    return (
+        <>
+            <h3 style={{ color: 'black' }}>Ex 1.1</h3>
+            <p>ID: {id}</p>
+            <p>Name: {name}</p>
+        </>
+    );
+}
